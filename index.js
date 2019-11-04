5f8214b3185fa582d4b7f06c32f6bb50866e015e
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-11-04 15:14:11 +0100

    Reformat code

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const express = require("express");
-const { createReadStream } = require("fs");
-const bodyParser = require("body-parser");
-const app = express();
-const port = 3000;
+const express = require('express')
+const { createReadStream } = require('fs')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
 
 const users = {
-  john: "password",
-  mohammed: "123456"
-};
+  john: 'password',
+  mohammed: '123456'
+}
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-  createReadStream("index.html").pipe(res);
-});
+app.get('/', (req, res) => {
+  createReadStream('index.html').pipe(res)
+})
 
-app.post("/login", (req, res) => {
-  const password = users[req.body.username];
+app.post('/login', (req, res) => {
+  const password = users[req.body.username]
   if (password === req.body.password) {
-    res.send("Hello " + req.body.username);
+    res.send('Hello ' + req.body.username)
   } else {
-    res.redirect("/");
+    res.redirect('/')
   }
-});
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
