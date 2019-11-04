827be7d3b699ce2ef14fac44a30d5234bfce3c2a
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-11-04 15:06:57 +0100

    Add login form with username

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const { createReadStream } = require("fs");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  createReadStream("index.html").pipe(res);
+});
+
+app.post("/login", (req, res) => {
+  res.end("Hello " + req.body.username);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
