3c76055f04a9652b55af519ee47e55d019872688
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-11-04 15:32:03 +0100

    Set username cookie

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const { createReadStream } = require('fs')
 const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
+const cookieParser = require('cookie-parser')
 
+const port = 3000
 const users = {
-  john: 'password',
-  mohammed: '123456'
+  bo: 'pass',
+  yu: '123'
 }
 
+const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
   createReadStream('index.html').pipe(res)
@@ -18,7 +20,11 @@ app.get('/', (req, res) => {
 app.post('/login', (req, res) => {
   const password = users[req.body.username]
   if (password === req.body.password) {
-    res.send('Hello ' + req.body.username)
+    res.cookie('username', req.body.username)
+  }
+
+  if (users[req.cookies.username]) {
+    res.send('Hello ' + req.cookies.username)
   } else {
     res.redirect('/')
   }
