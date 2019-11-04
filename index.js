ea422af3816c03117c6ad3e456fd6b3b4a6dd56f
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-11-04 15:11:34 +0100

    Add password

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+const users = {
+  john: "password",
+  mohammed: "123456"
+};
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -11,7 +16,12 @@ app.get("/", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  res.end("Hello " + req.body.username);
+  const password = users[req.body.username];
+  if (password === req.body.password) {
+    res.send("Hello " + req.body.username);
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
