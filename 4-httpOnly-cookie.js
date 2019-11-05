4d0a36ef8d6988b8d3d87fd72914485478eb2ec4
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-11-05 10:44:42 +0100

    Add transfer endpoint

--- 4-httpOnly-cookie.js
+++ 4-httpOnly-cookie.js
@@ -4,15 +4,10 @@ const cookieParser = require('cookie-parser')
 const crypto = require('crypto')
 const { createReadStream } = require('fs')
 
-const port = 3000
+const port = 3004
 const users = {
   bo: 'pass2',
   yu: '123'
-}
-
-const balances = {
-  bo: 500,
-  yu: 1000
 }
 
 const sessions = {}
@@ -24,23 +19,7 @@ app.use(cookieParser('secret that is really hard to guess'))
 
 app.get('/', (req, res) => {
   if (req.signedCookies.sessionId) {
-    const username = sessions[req.signedCookies.sessionId]
-    const balance = balances[username]
-    const html = `
-      <p>Your balance is $${balance}</p>
-      <form action="/transfer" method="post">
-        <label>
-          Send amount:
-          <input name="amount" type="text" />
-        </label>
-        <label>
-          To:
-          <input name="to" type="text" />
-        </label>
-        <button type="submit">Transfer</button>
-      </form>
-    `
-    res.send(html)
+    res.send('Hello ' + sessions[req.signedCookies.sessionId])
   } else {
     createReadStream('login.html').pipe(res)
   }
@@ -55,19 +34,6 @@ app.post('/login', (req, res) => {
       httpOnly: true
     })
     sessions[sessionId] = req.body.username
-    res.redirect('/')
-  } else {
-    res.send('fail!')
-  }
-})
-
-app.post('/transfer', (req, res) => {
-  const to = req.body.to
-  if (req.signedCookies.sessionId && balances[to]) {
-    const from = sessions[req.signedCookies.sessionId]
-    const amount = Number(req.body.amount)
-    balances[from] -= amount
-    balances[to] += amount
     res.redirect('/')
   } else {
     res.send('fail!')
